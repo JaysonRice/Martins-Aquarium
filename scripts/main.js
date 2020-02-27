7804e086144cd9980ceb5d1ef3fabb209e773777
root <root@DESKTOP-2KLJLBF.localdomain>
2020-02-27 13:40:11 -0600

    Organized Javascript directories

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 import initializeDetailButtonEvents from './dialogs.js'
-import { useFish } from './FishDataProvider.js'
-import FishList from './FishList.js'
-
+import { useFish } from './fish/FishDataProvider.js'
+import FishList from './fish/FishList.js'
 // initializeDetailButtonEvents()
 
 const arrayOfTheFishObjects = useFish()
