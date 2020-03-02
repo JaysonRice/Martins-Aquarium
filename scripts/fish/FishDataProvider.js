9930ab863a3a8a76e820c2454275392001681a48
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-02 15:43:07 -0600

    Filtered fish in order of divisible lengths.

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -1,16 +1,16 @@
 const fishCollection = [
-    {   
+    {
         name: "Lightbulb",
         image: 'https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/fish/group/anglerfish_thumb.jpg',
-        length: "2 ft",
+        size: 23,
         species: "Anglerfish",
         food: ["Crustaceans, squids, snails"],
         location: "Deep Antarctic Ocean"
     },
     {
         name: "Snuffles",
-        image:'https://itsnature.org/wp-content/uploads/2008/02/070209-goblin-shark_big.jpg',
-        length: "4 ft",
+        image: 'https://itsnature.org/wp-content/uploads/2008/02/070209-goblin-shark_big.jpg',
+        size: 38,
         species: "Goblin shark",
         food: ["Small fish, rays, squid"],
         location: "Honshu, Japan"
@@ -18,7 +18,7 @@ const fishCollection = [
     {
         name: "Blubb",
         image: 'https://m.liveaquaria.com/images/categories/large/lg_107557_ORA_Stubby_Ocellaris_Clownfish.jpg',
-        length: "3 in",
+        size: 3,
         species: "Clownfish",
         food: ["Algae, zooplankton"],
         location: "Great Barrier Reef"
@@ -26,7 +26,7 @@ const fishCollection = [
     {
         name: "Pouty",
         image: 'https://thumbs-prod.si-cdn.com/2tSlDBd2Jh97_c7vNsn58HAhkkY=/1072x720/filters:no_upscale()/https://public-media.si-cdn.com/filer/d6/93/d6939718-4e41-44a8-a8f3-d13648d2bcd0/c3npbx.jpg',
-        length: "1 ft",
+        size: 5,
         species: "Pufferfish",
         food: ["Invertebrates, algae"],
         location: "Indian Ocean"
@@ -34,7 +34,7 @@ const fishCollection = [
     {
         name: "Flapjack",
         image: 'https://vignette.wikia.nocookie.net/wildkratts/images/7/72/Atlantic_Flying_Fish_RL.PNG/revision/latest?cb=20191021143355',
-        length: "1 ft",
+        size: 12,
         species: "Flying fish",
         food: ["Plankton, crustaceans "],
         location: "Pacific Ocean"
@@ -42,7 +42,7 @@ const fishCollection = [
     {
         name: "Sonny",
         image: 'https://thumbs-prod.si-cdn.com/79xDamFZC8yUv7WeBtXgSzUXe0w=/fit-in/1600x0/https://public-media.si-cdn.com/filer/94/c1/94c102c0-1929-4ff6-b6ff-fd5d5ae036ae/42-25125374.jpg',
-        length: "4 ft",
+        size: 40,
         species: "Sunfish",
         food: ["Squid", "jellyfish"],
         location: "Palos Verdes Peninsula"
@@ -52,3 +52,42 @@ const fishCollection = [
 export const useFish = () => {
     return fishCollection.slice()
 }
+
+
+export const mostHolyFish = () => {
+    let holyFishArray = []
+
+    for (const holyFish of fishCollection) {
+        if (holyFish.size % 3 === 0) {
+        holyFishArray.push(holyFish)
+           
+        }
+    }
+    return holyFishArray
+}
+
+export const soldierFish = () => {
+    let soldierFishArray = []
+    
+    for (const soldierFish of fishCollection) {
+        if (soldierFish.size % 5 === 0) {
+        soldierFishArray.push(soldierFish)
+           
+        }
+    }
+    return soldierFishArray
+}
+
+
+export const nonHolyFish  = () => {
+    let nonHolyFishArray = []
+    
+    for (const nonHolyFish of fishCollection) {
+        if (nonHolyFish.size % 5 !== 0 && nonHolyFish.size % 3 !== 0) {
+        nonHolyFishArray.push(nonHolyFish)
+           
+        }
+    }
+    return nonHolyFishArray
+}
+
--- scripts/fish/FishList.js
+++ scripts/fish/FishList.js
@@ -1,14 +1,28 @@
-import { useFish } from './FishDataProvider.js'
+import { useFish, mostHolyFish, soldierFish, nonHolyFish } from './FishDataProvider.js'
 import Fish from './Fish.js'
 
+let fishHTMLRepresentations = ""
 const FishList = () => {
 
     // Get a reference to the `<article class="content">` element
     const contentElement = document.querySelector(".fishList")
-    const fishes = useFish()
+
+    let fishes = mostHolyFish()
 
     // Generate all of the HTML for all of the fish
-    let fishHTMLRepresentations = ""
+    
+    for (const fishObjects of fishes) {
+        fishHTMLRepresentations += Fish(fishObjects)
+    }
+
+     fishes = soldierFish()
+
+    for (const fishObjects of fishes) {
+        fishHTMLRepresentations += Fish(fishObjects)
+    }
+
+    fishes = nonHolyFish()
+
     for (const fishObjects of fishes) {
         fishHTMLRepresentations += Fish(fishObjects)
     }
@@ -20,3 +34,4 @@ const FishList = () => {
 }
 
 export default FishList
+
